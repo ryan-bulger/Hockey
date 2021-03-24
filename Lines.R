cc69c9cfadc543ba85754b750da5fe9c8ac92451
Ryan Bulger <ryan.d.bulger@gmail.com>
2021-03-24 15:35:31 -0600

    Final code to pull all lines for Forwards, Defence, & Goalies.  Still need to create PP lists.

--- Lines.R
+++ Lines.R
@@ -1,51 +1,51 @@
 library(tidyverse)
 library(rvest)
+setwd("C:/Users/ryan_bulger/projects/Hockey")
 
-# TEAM NAMES FUNCTION
+# Pulls line data from DFO for each team
 team <- function (n) {
-   tm <- read_html("https://www.dailyfaceoff.com/teams/") %>%
+   tm <<- read_html("https://www.dailyfaceoff.com/teams/") %>%
         html_nodes(".team-list-name") %>%
         html_text() %>%
         tolower() %>%
         gsub(pattern = " ",replacement = "-")
    tm_page <<- str_glue("https://www.dailyfaceoff.com/teams/",{tm[n]},"/line-combinations/")
 }
-    
-team(19)
-tm_page
 
-# LINES FUNCTION
-line_create <- function(node,n) {
+# Pulls line info for a single team from DFO
+line_create <- function(node) {
     read_html(tm_page) %>%
         html_nodes(node) %>%
         html_nodes(".player-name") %>%
-        html_text %>%
-        matrix(n) %>%
-        t() %>%
-        as_tibble()
+        html_text
 }
 
-lines <- read.csv("nodes.csv") %>%
-    pmap(line_create)
-
-FWD <- lines[[1]]
-DEF <- lines [[2]]
-PP <- rbind(cbind(lines[[3]],lines[[4]]),cbind(lines[[5]],lines[[6]]))
-G <- lines[[7]]
-INJ <- tryCatch(lines[[8]],error=function(e) NA)
-
-            # Pull injury type ("IR", "DTD", "OUT")
-            # Sorted by Position (from NHL API)
-    # FUNC TO SEARCH LINE POSITION IN ABOVE TABLE
-    # FULL TEAM TABLE
-        # Used to display full team for visualization
-        # 3 x 7 matrix
-        # No col headers
-        # F on top, D lines below and split L & R w/ G in between D
-
-
-easy <- function(){
-    return(list(fnm="ryan",lnm="bulger"))
+# Creates tibble of team lines stacked by: FWD, D, G
+lines <- function (n) {
+    return({
+        read.csv("nodes.csv") %>%
+            pmap(line_create)%>%
+            unlist() %>%
+            tibble(.name_repair=~tm[n]) 
+        })
 }
-easy <- easy()
-easy$fnm
+
+df = tibble(read_csv("positions.csv"))
+
+#for (i in 1:length(tm)) 
+for (i in 11:13) {
+    team(i)
+    df<-bind_cols(df,lines(i))
+}
+#print(df)
+write_csv(df,"lines.csv")
+#INJURIES
+    #tryCatch(lines[[8]],error=function(e) NA)
+    # Pull injury type ("IR", "DTD", "OUT")
+    # Sorted by Position (from NHL API)
+
+# FULL TEAM TABLE
+    # Used to display full team for visualization
+    # 3 x 7 matrix
+    # No col headers
+    # F on top, D lines below and split L & R w/ G in between D
